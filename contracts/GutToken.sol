054cda4697bebca2b68540b625a88131fc160a93
Gut <thiramate@nimblehq.co>
2021-11-07 13:52:36 +0700

    Add transfer function with unit test

--- contracts/GutToken.sol
+++ contracts/GutToken.sol
@@ -3,8 +3,13 @@ pragma solidity ^0.8.4; // Define solidity version
 // Declare the smart contract
 contract GutToken {
     uint256 internal totalSupply_;
+    string public constant name = "GutToken";
+    string public constant symbol = "GUT";
+    uint8 public constant decimals = 18;
 
     mapping(address => uint256) internal balances;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
     // Constructor function
     constructor(uint256 _initialSupply) {
@@ -27,5 +32,24 @@ contract GutToken {
      */
     function balanceOf(address _addr) public view returns (uint256) {
         return balances[_addr];
+    }
+
+    /**
+     * @dev Transfers tokekn to specified address from msg.sender
+     * @param _to The receiver address
+     * @param _value The amount of token to be transferred
+     * @return boolean Transfer status (true = success, false = unsuccess)
+     */
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        // Condition to check if msg.sender has enough balance to transfer amount of _value
+        require(balances[msg.sender] >= _value, "insufficient funds");
+
+        balances[msg.sender] = balances[msg.sender] - _value; // Subtract token from sender by _value
+        balances[_to] = balances[_to] + _value; // Add token to recipient by _value
+
+        // According to ERC20 standard transfer function MUST fire the Transfer event
+        emit Transfer(msg.sender, _to, _value);
+
+        return true; // Return the transfer status (success)
     }
 }
--- test/GutToken.js
+++ test/GutToken.js
@@ -1,33 +1,144 @@
 const GutToken = artifacts.require("GutToken");
 
 contract('GutToken', (accounts) => {
-  let gutToken;
-
-  before(async() => {
-    gutToken = await GutToken.deployed();
-  });
-
   describe('Initialize', () => {
-    it('returns correct total supply', async() => {
-      const totalSupply = await gutToken.totalSupply();
+    let gutToken;
+    before(async() => {
+      gutToken = await GutToken.deployed();
+    });
 
+    it('sets correct total supply', async() => {
+      const totalSupply = await gutToken.totalSupply();
       assert.equal(totalSupply.toNumber(), 1000000);
+    });
+
+    it('sets correct token name', async() => {
+      const tokenName = await gutToken.name();
+      assert.equal(tokenName, 'GutToken');
+    });
+
+    it('sets correct token symbol', async() => {
+      const tokenSymbol = await gutToken.symbol();
+      assert.equal(tokenSymbol, 'GUT');
+    });
+
+    it('sets correct token decimals', async() => {
+      const tokenDecimals = await gutToken.decimals();
+      assert.equal(tokenDecimals, 18);
     });
   });
 
   describe('balanceOf', () => {
     describe('given initial supply from initialize step', () => {
+      let gutToken;
+      before(async() => {
+        gutToken = await GutToken.deployed();
+      });
+
       it('returns total supply as a balance from deployer address', async() => {
         const balance = await gutToken.balanceOf(accounts[0]);
-  
         assert.equal(balance.toNumber(), 1000000);
       });
 
       it('returns 0 balance from non deployer address', async() => {
         const balance = await gutToken.balanceOf(accounts[1]);
-
         assert.equal(balance.toNumber(), 0);
+      });
+    });
+  });
+
+  describe('transfer', () => {
+    describe('given valid recipient address', () => {
+      let gutToken;
+      before(async() => {
+        gutToken = await GutToken.deployed();
       })
+
+      describe('given 0 value', () => {
+        let transaction;
+        before(async() => {
+          transaction = await gutToken.transfer(accounts[1], 0);
+        });
+
+        it('returns true', async() => {
+          const status = await gutToken.transfer.call(accounts[1], 100000);
+          assert.isTrue(status);
+        });
+
+        it('emits a Transfer event', () => {
+          assert.equal(transaction.logs[0].event, 'Transfer');
+        });
+
+        it('returns correct balance from msg.sender address', async() => {
+          const balance = await gutToken.balanceOf(accounts[0]);
+          assert.equal(balance.toNumber(), 1000000);
+        });
+
+        it('returns 0 balance from recipient address', async() => {
+          const balance = await gutToken.balanceOf(accounts[1]);
+          assert.equal(balance.toNumber(), 0);
+        });
+      });
+
+      describe('given sufficient value', () => {
+        let transaction;
+        before(async() => {
+          transaction = await gutToken.transfer(accounts[1], 100000);
+        });
+
+        it('returns true', async() => {
+          const status = await gutToken.transfer.call(accounts[1], 100000);
+          assert.isTrue(status);
+        });
+
+        it('emits a Transfer event', () => {
+          assert.equal(transaction.logs[0].event, 'Transfer');
+        });
+
+        it('returns correct balance from msg.sender address', async() => {
+          const balance = await gutToken.balanceOf(accounts[0]);
+          assert.equal(balance.toNumber(), 900000);
+        });
+
+        it('returns correct balance from recipient address', async() => {
+          const balance = await gutToken.balanceOf(accounts[1]);
+          assert.equal(balance.toNumber(), 100000);
+        });
+      });
+  
+      describe('given insufficient value', () => {
+        it('throws an error with correct error message', async() => {
+          try {
+            await gutToken.transfer(accounts[1], 999999999);
+            assert.fail('Transaction should throw an error');
+          } catch(err) {
+            assert.equal(err.reason, 'insufficient funds');
+          }
+        });
+      });
+  
+      describe('given negative value', () => {
+        it('throw an error with correct error message', async() => {
+          try {
+            await gutToken.transfer(accounts[1], -100);
+            assert.fail('Transaction should throw an error');
+          } catch(err) {
+            assert.equal(err.reason, 'value out-of-bounds');
+          }
+        });
+      });
+    });
+
+    describe('given invalid recipient address', () => {
+      it('throw an error with correct error message', async() => {
+        const gutToken = await GutToken.deployed();
+        try {
+          await gutToken.transfer('invalid address', 100000);
+          assert.fail('Transaction should throw an error');
+        } catch(err) {
+          assert.equal(err.value, 'invalid address');
+        }
+      });
     });
   });
 });
