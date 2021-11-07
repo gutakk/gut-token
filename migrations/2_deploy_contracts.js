81b3f0bd4126a4685ee9ad455fdb3d11f6fd2f43
Gut <thiramate@nimblehq.co>
2021-11-08 02:43:14 +0700

    Add LendingVault migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 const GutToken = artifacts.require("GutToken");
+const LendingVault = artifacts.require("LendingVault");
 
 module.exports = function (deployer) {
-  deployer.deploy(GutToken, 1000000);
+  deployer.deploy(GutToken, 1000000).then(function() {
+    return deployer.deploy(LendingVault, GutToken.address);
+  });
 };
