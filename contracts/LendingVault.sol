7e63f4c27af86aa4589b1ca8e8290093a5160199
Gut <thiramate@nimblehq.co>
2021-11-11 01:31:02 +0700

    Add withdraw function and unit tests

--- contracts/LendingVault.sol
+++ contracts/LendingVault.sol
@@ -8,12 +8,17 @@ contract LendingVault {
     DGutToken public dGutToken;
 
     event Deposit(uint256 _amount);
+    event Withdraw(uint256 _amount);
 
     constructor(GutToken _gutTokenContract, DGutToken _dGutTokenContract) {
         gutToken = _gutTokenContract;
         dGutToken = _dGutTokenContract;
     }
 
+    /**
+     * @dev Deposits GutToken to lending vault and receive dGutToken as a debt token
+     * @param _amount Amount of token to be deposited
+    */
     function deposit(uint256 _amount) external {
         // Transfer GutToken to lending vault
         gutToken.transferFrom(msg.sender, address(this), _amount);
@@ -22,5 +27,19 @@ contract LendingVault {
 
         // Emit deposit event
         emit Deposit(_amount);
+    }
+
+    /**
+     * @dev Withdraws GutToken from lending vault and remove dGutToken
+     * @param _amount Amount of token to be withdrawn
+    */
+    function withdraw(uint256 _amount) external {
+        // Burns debt tokens from msg.sender account by _amount
+        dGutToken.burn(msg.sender, _amount);
+        // Transfers Gut Token from contract to msg.sender account by _amount
+        gutToken.transfer(msg.sender, _amount);
+
+        // Emit withdraw event
+        emit Withdraw(_amount);
     }
 }
--- test/LendingVault.js
+++ test/LendingVault.js
@@ -2,6 +2,9 @@ const GutToken = artifacts.require("GutToken");
 const DGutToken = artifacts.require("DGutToken");
 const LendingVault = artifacts.require("LendingVault");
 
+const depositAmount = 100;
+const withdrawAmount = 50;
+
 contract('LendingVault', (accounts) => {
   before(async() => {
     gutToken = await GutToken.deployed();
@@ -23,26 +26,24 @@ contract('LendingVault', (accounts) => {
   describe('deposit', () => {
     describe('given 0 amount', () => {
       before(async() => {
-        depositAmount = 0;
         await gutToken.approve(lendingVault.address, 100);
-        transaction = await lendingVault.deposit(depositAmount);
+        transaction = await lendingVault.deposit(0);
       });
   
       it('returns correct balance from lending vault after deposit', async() => {
         const contractBalance = await gutToken.balanceOf(lendingVault.address);
-        assert.equal(contractBalance.toNumber(), depositAmount);
+        assert.equal(contractBalance.toNumber(), 0);
       });
   
       it('emits Deposit event', async() => {
         assert.equal(transaction.logs.length, 1);
         assert.equal(transaction.logs[0].event, 'Deposit');
-        assert.equal(transaction.logs[0].args._amount, depositAmount);
+        assert.equal(transaction.logs[0].args._amount, 0);
       });
     });
 
     describe('given valid amount', () => {
       before(async() => {
-        depositAmount = 100;
         await gutToken.approve(lendingVault.address, depositAmount);
         transaction = await lendingVault.deposit(depositAmount);
       });
@@ -69,6 +70,101 @@ contract('LendingVault', (accounts) => {
         try {
           await gutToken.approve(lendingVault.address, 100);
           await lendingVault.deposit(-100);
+          assert.fail('Transaction should throw an error');
+        } catch(err) {
+          assert.equal(err.reason, 'value out-of-bounds');
+        }
+      });
+    });
+  });
+
+  describe('withdraw', () => {
+    describe('given 0 amount', () => {
+      before(async() => {
+        accountBalance = await gutToken.balanceOf(accounts[0])
+        transaction = await lendingVault.withdraw(0);
+      });
+
+      it('emits Withdraw event', async() => {
+        assert.equal(transaction.logs.length, 1);
+        assert.equal(transaction.logs[0].event, 'Withdraw');
+        assert.equal(transaction.logs[0].args._amount, 0);
+      });
+
+      it('does NOT receive GutToken', async() => {
+        const finalBalance = await gutToken.balanceOf(accounts[0]);
+        assert.equal(finalBalance.toNumber(), accountBalance.toNumber());
+      });
+
+      it('does NOT remove dGutToken', async() => {
+        const debtBalance = await dGutToken.balanceOf(accounts[0]);
+        assert.equal(debtBalance.toNumber(), depositAmount);
+      });
+    });
+
+    describe('given valid amount', () => {
+      before(async() => {
+        accountBalance = await gutToken.balanceOf(accounts[0])
+        transaction = await lendingVault.withdraw(withdrawAmount);
+      });
+
+      it('emits Withdraw event', async() => {
+        assert.equal(transaction.logs.length, 1);
+        assert.equal(transaction.logs[0].event, 'Withdraw');
+        assert.equal(transaction.logs[0].args._amount, withdrawAmount);
+      });
+
+      it('receives correct amount of GutToken', async() => {
+        const finalBalance = await gutToken.balanceOf(accounts[0]);
+        assert.equal(finalBalance.toNumber(), accountBalance.toNumber() + withdrawAmount);
+      });
+
+      it('removes correct amount of dGutToken', async() => {
+        const debtBalance = await dGutToken.balanceOf(accounts[0]);
+        assert.equal(debtBalance.toNumber(), depositAmount - withdrawAmount);
+      });
+    });
+
+    describe('given amount exceeds debt token balance', () => {
+      before(async() => {
+        accountBalance = await gutToken.balanceOf(accounts[0])
+        debtBalance = await dGutToken.balanceOf(accounts[0])
+      });
+
+      it('throw an error with correct error message', async() => {
+        try {
+          await lendingVault.withdraw(9999);
+          assert.fail('Transaction should throw an error');
+        } catch(err) {
+          assert.equal(err.reason, 'burn amount exceeds balance');
+        }
+      });
+
+      it('does NOT transfer GutToken from contract', async() => {
+        try {
+          await lendingVault.withdraw(9999);
+          assert.fail('Transaction should throw an error');
+        } catch(err) {
+          const finalBalance = await gutToken.balanceOf(accounts[0]);
+          assert.equal(finalBalance.toNumber(), accountBalance.toNumber());
+        }
+      });
+
+      it('does NOT remove dGutToken from account', async() => {
+        try {
+          await lendingVault.withdraw(9999);
+          assert.fail('Transaction should throw an error');
+        } catch(err) {
+          const finalDebtBalance = await dGutToken.balanceOf(accounts[0]);
+         assert.equal(finalDebtBalance.toNumber(), debtBalance);
+        }
+      });
+    });
+
+    describe('given negative amount', () => {
+      it('throw an error with correct error message', async() => {
+        try {
+          await lendingVault.withdraw(-100);
           assert.fail('Transaction should throw an error');
         } catch(err) {
           assert.equal(err.reason, 'value out-of-bounds');
